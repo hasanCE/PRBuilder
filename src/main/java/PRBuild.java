0772e9aed0bc788e68bdbb0a81676e59fd1f0fb0
hasan <testerselenium34@gmail.com>
2021-08-18 00:42:50 +0300

    pr builder testing

--- src/main/java/PRBuild.java
+++ src/main/java/PRBuild.java
@@ -3,5 +3,6 @@ public class PRBuild {
     public static void main(String[] args){
         PRBuildTest prBuildTest = new PRBuildTest();
         System.out.printf(prBuildTest.toString());
+        System.out.println(prBuildTest.whatHappened());
     }
 }
--- src/main/java/PRBuildTest.java
+++ src/main/java/PRBuildTest.java
@@ -3,4 +3,9 @@ public class PRBuildTest {
    public PRBuildTest(){
        System.out.println("tester");
    }
+
+   public String whatHappened(){
+
+       return "yeah sth happened";
+   }
 }
