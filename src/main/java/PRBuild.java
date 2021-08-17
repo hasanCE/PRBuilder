65b2e09db49dfb7daff3a9abb1b2c6eed7763402
hasan <testerselenium34@gmail.com>
2021-08-18 00:41:39 +0300

    pr builder testing

--- src/main/java/PRBuild.java
+++ src/main/java/PRBuild.java
@@ -1,9 +1,6 @@
 public class PRBuild {
 
     public static void main(String[] args){
-        System.out.println("first pr request started");
-        System.out.println("first pr request finished");
-        System.out.println("first pr request still going on");
         PRBuildTest prBuildTest = new PRBuildTest();
         System.out.printf(prBuildTest.toString());
     }
