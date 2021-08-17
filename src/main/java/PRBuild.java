b784bacc03e45ceaba7e3da1efe3eb71cf3a35a8
hasan <testerselenium34@gmail.com>
2021-08-17 15:35:29 +0300

    pr builder testing

--- src/main/java/PRBuild.java
+++ src/main/java/PRBuild.java
@@ -1,6 +1,10 @@
 public class PRBuild {
 
     public static void main(String[] args){
+        System.out.println("first pr request started");
+
         System.out.println("first pr request and builder in jenkins");
+        System.out.println("first pr request finished");
+
     }
 }
